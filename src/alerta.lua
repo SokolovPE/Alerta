0275d8ba24ea45eff11516c6262b989726ad3f55
Pavel Sokolov <51756629+SokolovPE@users.noreply.github.com>
2025-01-19 05:56:12 +0300

    V1.0.2
    * Elite sound and additional threat detection
    
    * Improved condig section
    
    * Improved .toc
    
    * remove OnUpdate and general rewrite
    
    * add minimap icon
    
    * version update
    
    * small cleanup

--- src/alerta.lua
+++ src/alerta.lua
@@ -1,220 +1,155 @@
-ALERTA_NAME = "Alerta"
-COLOR_CODES = {
-	Success = "FF00FF0D",
-	Info = "FF0085DD"
-}
 local alerta = CreateFrame("Frame")
 local mobListSize = 30
 
--- TODOS
--- custom sound for elite mobs
--- allow to select multiple sounds
--- allow to set sound path manually
--- send addon event message to let other addons to communicate
+-- Tables to track mobs and threats
 local mobs = {}
 local threats = {}
 local playerAwareTargets = {}
 
+-- Event handler
 function alerta:OnEvent(event, ...)
-	self[event](self, event, ...)
+    self[event](self, event, ...)
 end
 
-function alerta:OnUpdate()
-	-- for each mob detected
-	for value in pairs(mobs) do
-		if (not UnitExists(value) or UnitIsUnit(value, "target")) then alerta:removeFromTable(mobs, value) end
-		local threatStatus = UnitThreatSituation("player", value)
-		if (threatStatus ~= nil and threatStatus >= 2) then
-			-- print("threat")
-			alerta:appendThreat(value)
-		end
-	end
-
-	-- local statusAny = UnitThreatSituation("player")
-	-- -- if state is still same - do nothing
-	-- if (lastThreatSituation == statusAny) then return end;
-	-- -- update last threat situation
-	-- lastThreatSituation = statusAny
-	-- if (statusAny == nil) then
-	-- 	print("Out of combat")
-	-- elseif (statusAny >= 2) then
-	-- 	print("Entered combat")
-	-- end
-	-- DEFAULT_CHAT_FRAME:AddMessage(statusAny, 1, 1, 1)
-end
-
+-- Addon loaded
 function alerta:ADDON_LOADED(_, addOnName)
-	if (addOnName ~= ALERTA_NAME) then return end
+    if addOnName ~= AlertaOptions.ALERTA_NAME then return end
 
-	local dbDefaults = {
-		sound = "Interface\\AddOns\\Alerta\\Sounds\\Alerta.ogg",
-		channel = "Master",
-		printAnotherOne = false
-	}
-	AlertaSettings = AlertaSettings or dbDefaults
-	Output("Loaded " .. WrapTextInColorCode("successfully", COLOR_CODES.Success))
+    -- Default settings
+    local dbDefaults = {
+        sound = "Interface\\AddOns\\Alerta\\Sounds\\Alerta.ogg",
+        eliteSound = "Interface\\AddOns\\Alerta\\Sounds\\AlertUnexpected.ogg",
+        channel = "Master",
+        printAnotherOne = false,
+        eliteSoundOn = false,
+        minimap = { hide = false }, -- Default to showing the minimap icon
+    }
+    AlertaSettings = AlertaSettings or dbDefaults
+
+    -- Initialize minimap icon
+    AlertaOptions:InitializeMinimapIcon()
+
+    Output("Loaded " .. WrapTextInColorCode("successfully", AlertaOptions.COLOR_CODES.Success))
 end
 
+-- Threat list updated
 function alerta:UNIT_THREAT_LIST_UPDATE(_, unitId)
-	if (unitId == nil) then return end
-	-- local uid = UnitGUID(unitId)
-	local threatStatus = UnitThreatSituation("player", unitId)
-	if (threatStatus == nil or threatStatus < 2) then
-		-- print("Mob "..uid.." is no longer a danger")
-		alerta:removeThreat(unitId)
-	elseif (threatStatus >= 2) then
-		-- print("Mob "..uid.." changed threat level to "..threatStatus)
-		alerta:appendThreat(unitId)
-	end
-	-- DEFAULT_CHAT_FRAME:AddMessage("UnitId: "..unitId, 1, 1, 1);
+    if not unitId then return end
+
+    local threatStatus = UnitThreatSituation("player", unitId)
+    if not threatStatus or threatStatus < 2 then
+        self:RemoveThreat(unitId)
+    else
+        self:AppendThreat(unitId)
+    end
 end
 
--- New mob detected
+-- New mob detected via nameplate
 function alerta:NAME_PLATE_UNIT_ADDED(_, unitId)
-	alerta:appendToTable(mobs, unitId)
+    self:TrackMob(unitId)
 end
 
--- Out of combat event
-function alerta:PLAYER_REGEN_ENABLED(_)
-	alerta:clearTable(threats)
+-- Player exits combat
+function alerta:PLAYER_REGEN_ENABLED()
+    self:ClearTable(threats)
 end
 
--- Unit combat event (alsao detects AOE hits)
-function alerta:UNIT_COMBAT(_, unitTarget, event, flagText, amount, schoolMask)
-	if (unitTarget ~= "player" and unitTarget ~= nil) then
-		local uid = UnitGUID(unitTarget)
-		-- print(uid)
-		-- print("[COMBAT] Player is now aware of "..uid)
-		alerta:appendToTable(playerAwareTargets, uid)
-	end
+-- Mob attacks player (even without nameplates)
+function alerta:COMBAT_LOG_EVENT_UNFILTERED()
+    local _, subEvent, _, _, _, _, _, destGUID, _, _, _, _ = CombatLogGetCurrentEventInfo()
+    if subEvent == "SWING_DAMAGE" or subEvent == "SPELL_DAMAGE" then
+        if destGUID == UnitGUID("player") then
+            local sourceGUID = select(8, CombatLogGetCurrentEventInfo())
+            if sourceGUID and not UnitIsUnit(sourceGUID, "target") then
+                self:TrackMob(sourceGUID)
+            end
+        end
+    end
 end
 
--- Unit casted a spell
-function alerta:UNIT_SPELLCAST_SENT(_, unit, target, castGUID, spellID)
-	if (target ~= "player" and target ~= nil) then
-		local uid = UnitGUID("target")
-		-- print("[SPELL] Player is now aware of "..uid)
-		alerta:appendToTable(playerAwareTargets, uid)
-	end
+-- Track a new mob
+function alerta:TrackMob(unitId)
+    if not UnitExists(unitId) or UnitIsUnit(unitId, "target") then return end
+    self:AppendToTable(mobs, unitId)
 end
 
+-- Append unit to threat list
+function alerta:AppendThreat(unitId)
+    local uid = UnitGUID(unitId)
+    if not uid or self:TableContains(playerAwareTargets, uid) or UnitIsUnit(unitId, "target") then return end
+
+    if self:AppendToTable(threats, uid) then
+        self:PlaySound(unitId)
+        if AlertaSettings.printAnotherOne then
+            Output("Another one! " .. WrapTextInColorCode(uid, AlertaOptions.COLOR_CODES.Info))
+        end
+    end
+end
+
+-- Remove unit from threat list
+function alerta:RemoveThreat(unitId)
+    self:RemoveFromTable(threats, unitId)
+end
+
+-- Play sound based on mob type
+function alerta:PlaySound(unitId)
+    local soundFile = AlertaSettings.sound
+    if AlertaSettings.eliteSoundOn and (UnitClassification(unitId) == "elite" or UnitClassification(unitId) == "rareelite") then
+        soundFile = AlertaSettings.eliteSound
+    end
+    PlaySoundFile(soundFile, AlertaSettings.channel)
+end
+
+-- Utility functions
+function alerta:AppendToTable(table, key)
+    if not key or self:TableContains(table, key) then return false end
+    if self:GetTableSize(table) >= mobListSize then
+        self:PopTable(table)
+    end
+    table[key] = true
+    return true
+end
+
+function alerta:RemoveFromTable(table, key)
+    table[key] = nil
+end
+
+function alerta:TableContains(table, key)
+    return table[key] ~= nil
+end
+
+function alerta:GetTableSize(table)
+    local count = 0
+    for _ in pairs(table) do count = count + 1 end
+    return count
+end
+
+function alerta:PopTable(table)
+    for key in pairs(table) do
+        table[key] = nil
+        break
+    end
+end
+
+function alerta:ClearTable(table)
+    for key in pairs(table) do
+        table[key] = nil
+    end
+end
+
+-- Output message to chat
+function Output(msg)
+    local printMsg = WrapTextInColorCode("[", AlertaOptions.COLOR_CODES.Info) ..
+        "Alerta |TInterface\\Icons\\Inv_misc_head_dragon_01:12|t" ..
+        WrapTextInColorCode("]", AlertaOptions.COLOR_CODES.Info) ..
+        " " .. msg
+    print(printMsg)
+end
+
+-- Register events
 alerta:RegisterEvent("ADDON_LOADED")
 alerta:RegisterEvent("UNIT_THREAT_LIST_UPDATE")
 alerta:RegisterEvent("NAME_PLATE_UNIT_ADDED")
 alerta:RegisterEvent("PLAYER_REGEN_ENABLED")
-alerta:RegisterEvent("UNIT_COMBAT")
-alerta:RegisterEvent("UNIT_SPELLCAST_SENT")
+alerta:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 alerta:SetScript("OnEvent", alerta.OnEvent)
-alerta:SetScript("OnUpdate", alerta.OnUpdate)
-
--- Append unit to threat list
-function alerta:appendThreat(unitId)
-	local uid = UnitGUID(unitId)
-	if (uid == nil) then return end
-	-- if player is aware of threat - do not append
-	if (alerta:tableContains(playerAwareTargets, uid) or UnitIsUnit(unitId, "target")) then return end
-
-	local newThreat = alerta:appendToTable(threats, uid)
-	-- alerta:printTable(threats)
-	-- print(newThreat)
-	if (newThreat == true) then
-		-- print("Another "..uid)
-		alerta:playSound()
-		if ((AlertaSettings.printAnotherOne or false) == true) then
-			Output("Another one! " .. WrapTextInColorCode(uid, COLOR_CODES.Info))
-		end
-	end
-end
-
--- Remove unit from threat list
-function alerta:removeThreat(unitId)
-	-- local uid = UnitGUID(unitId)
-	-- print("Mob "..uid.." is no longer a danger")
-	alerta:removeFromTable(threats, unitId)
-	-- alerta:printTable(threats)
-end
-
--- Play sound
-function alerta:playSound()
-	PlaySoundFile(AlertaSettings.sound, AlertaSettings.channel)
-end
-
-function alerta:appendToTable(table, targetId)
-	if targetId == nil then return false end
-	if not alerta:tableContains(table, targetId) then
-		-- print("Before")
-		-- alerta:printTable(table)
-		if alerta:getTableSize(table) >= mobListSize then
-			alerta:popTable(table)
-		end
-		-- local uid = UnitGUID(targetId)
-		alerta:addToTable(table, targetId, true)
-		-- print("After")
-		-- alerta:printTable(table)
-		return true
-	else
-		return false
-	end
-end
-
-function alerta:addToTable(table, key, value)
-	table[key] = value
-end
-
-function alerta:getFromTable(table, key)
-	return table[key]
-end
-
-function alerta:removeFromTable(table, key)
-	-- print("Before")
-	-- alerta:printTable(table)
-	table[key] = nil
-	-- print("After")
-	-- alerta:printTable(table)
-end
-
-function alerta:tableContains(table, key)
-	return table[key] ~= nil
-end
-
-function alerta:getTableSize(table)
-	local count = 0
-
-	for _ in pairs(table) do
-		count = count + 1
-	end
-
-	return count
-end
-
-function alerta:popTable(table)
-	for value in pairs(table) do
-		-- print("alerta:popTableped: "..value)
-		table[value] = nil
-		break
-	end
-end
-
-function alerta:printTable(table)
-	local count = 0
-
-	for value in pairs(table) do
-		print(value)
-	end
-
-	return count
-end
-
-function alerta:clearTable(table)
-	for value in pairs(table) do
-		table[value] = nil
-	end
-end
-
-function Output(msg)
-	local printMsg = WrapTextInColorCode("[", COLOR_CODES.Info) ..
-		"Alerta |TInterface\\Icons\\Inv_misc_head_dragon_01:12|t" ..
-		WrapTextInColorCode("]", COLOR_CODES.Info) ..
-		" " .. msg
-	print(printMsg)
-end
--- src/options.lua
+++ src/options.lua
@@ -2,6 +2,14 @@ local name, alerta = ...
 alerta.core = {}
 alerta.core.settings = {}
 local mod = alerta.core
+AlertaOptions = {
+    ALERTA_NAME = "Alerta",
+    COLOR_CODES = {
+        Success = "FF00FF0D",
+        Info = "FF0085DD"
+    }
+}
+
 
 -- Register shared sounds
 local LSM = LibStub("LibSharedMedia-3.0")
@@ -9,6 +17,11 @@ LSM:Register("sound", "Alerta:Alerta", "Interface\\AddOns\\Alerta\\Sounds\\Alert
 LSM:Register("sound", "Alerta:Unexpected", "Interface\\AddOns\\Alerta\\Sounds\\AlertUnexpected.ogg")
 LSM:Register("sound", "Alerta:AnotherOne", "Interface\\AddOns\\Alerta\\Sounds\\AnotherOne.ogg")
 LSM:Register("sound", "Alerta:HelloThere", "Interface\\AddOns\\Alerta\\Sounds\\HelloThere.ogg")
+
+-- Embed LibDBIcon-1.0
+local LibDBIcon = LibStub("LibDBIcon-1.0")
+local LibDataBroker = LibStub("LibDataBroker-1.1")
+local AceConfigDialog = LibStub("AceConfigDialog-3.0")
 
 local sounds = {}
 local channels = {
@@ -18,6 +31,33 @@ local channels = {
     Ambience = "Ambience",
     Dialog   = "Dialog"
 }
+
+-- Create a minimap button
+local minimapButton = LibDataBroker:NewDataObject(AlertaOptions.ALERTA_NAME, {
+    icon = "Interface\\Icons\\Inv_misc_head_dragon_01", -- Icon for the minimap button
+    OnClick = function(_, button)
+        if button == "LeftButton" then
+            -- Open the AceConfig dialog
+            AceConfigDialog:Open(AlertaOptions.ALERTA_NAME)
+        elseif button == "RightButton" then
+            -- Toggle minimap icon visibility
+            AlertaSettings.minimap.hide = not AlertaSettings.minimap.hide
+            LibDBIcon:Refresh(AlertaOptions.ALERTA_NAME, AlertaSettings.minimap)
+        end
+    end,
+    OnTooltipShow = function(tooltip)
+        tooltip:AddLine(AlertaOptions.ALERTA_NAME)
+        tooltip:AddLine("Left-click to open settings.")
+        tooltip:AddLine("Right-click to hide the minimap icon.")
+    end,
+    type = "launcher",
+})
+
+-- Initialize minimap icon
+function AlertaOptions:InitializeMinimapIcon()
+    AlertaSettings.minimap = AlertaSettings.minimap or { hide = false }
+    LibStub("LibDBIcon-1.0"):Register(AlertaOptions.ALERTA_NAME, minimapButton, AlertaSettings.minimap)
+end
 
 for sndName, path in next, LSM:HashTable("sound") do
     sounds[path] = sndName
@@ -34,64 +74,236 @@ end)
 
 mod.options = {
     type = "group",
-    name = ALERTA_NAME,
+    name = AlertaOptions.ALERTA_NAME,
+    childGroups = "tab", -- Use tabs for better organization
     args = {
-        sound = {
-            type = "select",
+        -- General Settings Tab
+        generalSettings = {
+            type = "group",
+            name = "General",
             order = 1,
-            values = sounds,
-            name = "Sound",
-            desc = "Set sound of alert",
-            set = function(_, val)
-                AlertaSettings.sound = val
-            end,
-            get = function(_)
-                return AlertaSettings.sound
-            end
+            args = {
+                -- Sound Settings Header
+                soundHeader = {
+                    type = "header",
+                    name = "Sound Settings",
+                    order = 10,
+                },
+                soundDesc = {
+                    type = "description",
+                    name = "Configure the sound settings for alerts.",
+                    fontSize = "medium",
+                    order = 11,
+                },
+                sound = {
+                    type = "select",
+                    order = 12,
+                    values = sounds,
+                    name = "Alert Sound",
+                    desc = "Choose the sound to play for alerts.",
+                    set = function(_, val)
+                        AlertaSettings.sound = val
+                    end,
+                    get = function(_)
+                        return AlertaSettings.sound
+                    end,
+                },
+                -- customSound = {
+                --     type = "input",
+                --     order = 13,
+                --     name = "Custom Sound Path",
+                --     desc =
+                --     "Enter the path to a custom sound file.\nExample: Interface\\AddOns\\Alerta\\Sounds\\Custom.ogg",
+                --     set = function(_, val)
+                --         AlertaSettings.customSound = val
+                --     end,
+                --     get = function()
+                --         return AlertaSettings.customSound or ""
+                --     end,
+                -- },
+                channel = {
+                    type = "select",
+                    order = 14,
+                    values = channels,
+                    name = "Sound Channel",
+                    desc = "Choose the channel to play the alert sound.",
+                    set = function(_, val)
+                        AlertaSettings.channel = val
+                    end,
+                    get = function(_)
+                        return AlertaSettings.channel
+                    end,
+                },
+                testSound = {
+                    type = "execute",
+                    name = "Preview Alert Sound",
+                    order = 15,
+                    func = function()
+                        PlaySoundFile(AlertaSettings.sound, AlertaSettings.channel)
+                    end,
+                },
+                -- Spacer
+                spacer1 = {
+                    type = "header",
+                    name = "",
+                    order = 16,
+                },
+                -- Chat Output Toggle
+                printAnotherOne = {
+                    type = "toggle",
+                    order = 17,
+                    width = "full",
+                    name = "Print Aggro to Chat",
+                    desc = "Output a message to chat when a new mob gains aggro.",
+                    set = function(_, val)
+                        AlertaSettings.printAnotherOne = val
+                    end,
+                    get = function(_)
+                        return AlertaSettings.printAnotherOne
+                    end,
+                },
+            },
         },
-        channel = {
-            type = "select",
+        -- Elite Mob Settings Tab
+        eliteSettings = {
+            type = "group",
+            name = "Elite Mobs",
             order = 2,
-            values = channels,
-            name = "Channel",
-            desc = "Channel to play sound at",
-            set = function(_, val)
-                AlertaSettings.channel = val
-            end,
-            get = function(_)
-                return AlertaSettings.channel
-            end
+            args = {
+                -- Elite Settings Header
+                eliteHeader = {
+                    type = "header",
+                    name = "Elite Mob Settings",
+                    order = 20,
+                },
+                eliteDesc = {
+                    type = "description",
+                    name = "Configure settings for elite mob alerts.",
+                    fontSize = "medium",
+                    order = 21,
+                },
+                eliteSoundOn = {
+                    type = "toggle",
+                    order = 22,
+                    width = "full",
+                    name = "Enable Elite Sound",
+                    desc = "Use a different sound for elite mobs.",
+                    set = function(_, val)
+                        AlertaSettings.eliteSoundOn = val
+                    end,
+                    get = function(_)
+                        return AlertaSettings.eliteSoundOn
+                    end,
+                },
+                eliteSound = {
+                    type = "select",
+                    order = 23,
+                    values = sounds,
+                    name = "Elite Alert Sound",
+                    desc = "Choose the sound to play for elite mob alerts.",
+                    set = function(_, val)
+                        AlertaSettings.eliteSound = val
+                    end,
+                    get = function(_)
+                        return AlertaSettings.eliteSound
+                    end,
+                    disabled = function()
+                        return not AlertaSettings.eliteSoundOn
+                    end,
+                    hidden = function()
+                        return not AlertaSettings.eliteSoundOn
+                    end,
+                },
+                testEliteSound = {
+                    type = "execute",
+                    name = "Preview Elite Sound",
+                    order = 24,
+                    func = function()
+                        PlaySoundFile(AlertaSettings.eliteSound, AlertaSettings.channel)
+                    end,
+                    disabled = function()
+                        return not AlertaSettings.eliteSoundOn
+                    end,
+                    hidden = function()
+                        return not AlertaSettings.eliteSoundOn
+                    end,
+                },
+            },
         },
-        test = {
-            type = "execute",
-            name = "Preview",
+        -- Minimap Settings Tab
+        minimapSettings = {
+            type = "group",
+            name = "Minimap",
             order = 3,
-            func = function()
-                PlaySoundFile(AlertaSettings.sound, AlertaSettings.channel)
-            end
+            args = {
+                -- Minimap Settings Header
+                minimapHeader = {
+                    type = "header",
+                    name = "Minimap Settings",
+                    order = 30,
+                },
+                minimapDesc = {
+                    type = "description",
+                    name = "Configure the minimap icon settings.",
+                    fontSize = "medium",
+                    order = 31,
+                },
+                minimapIcon = {
+                    type = "toggle",
+                    order = 32,
+                    name = "Show Minimap Icon",
+                    desc = "Toggle the minimap icon on or off.",
+                    get = function()
+                        return not AlertaSettings.minimap.hide
+                    end,
+                    set = function(_, value)
+                        AlertaSettings.minimap.hide = not value
+                        LibDBIcon:Refresh(AlertaOptions.ALERTA_NAME, AlertaSettings.minimap)
+                    end,
+                },
+            },
         },
-        printAnotherOne = {
-            type = "toggle",
+        -- Reset to Defaults
+        resetSettings = {
+            type = "group",
+            name = "Reset",
             order = 4,
-            width = "full",
-            name = "Print aggro to chat",
-            desc = "Output new mob alert to chat",
-            set = function(_, val)
-                AlertaSettings.printAnotherOne = val
-            end,
-            get = function(_)
-                return AlertaSettings.printAnotherOne
-            end
-        }
-    }
+            args = {
+                -- Reset Settings Header
+                resetHeader = {
+                    type = "header",
+                    name = "Reset Settings",
+                    order = 40,
+                },
+                resetDesc = {
+                    type = "description",
+                    name = "Reset all settings to their default values.",
+                    fontSize = "medium",
+                    order = 41,
+                },
+                resetButton = {
+                    type = "execute",
+                    order = 42,
+                    name = "Reset to Defaults",
+                    desc = "Reset all settings to default values.",
+                    func = function()
+                        AlertaSettings = nil
+                        ReloadUI()
+                    end,
+                },
+            },
+        },
+    },
 }
 
 -- Setup config
 LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable(name, mod.options, true)
-local _, categoryID = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(name)
+local _, categoryID = AceConfigDialog:AddToBlizOptions(name)
 
+-- Slash command to open config
 SLASH_ALERTA1 = "/alerta"
 SlashCmdList["ALERTA"] = function(_)
-    LibStub("AceConfigDialog-3.0"):Open(name)
-    LibStub("AceConfigDialog-3.0"):SelectGroup(name, categoryID)
+    LibStub("AceConfigDialog-3.0")
+    AceConfigDialog:Open(name)
+    AceConfigDialog:SelectGroup(name, categoryID)
 end
